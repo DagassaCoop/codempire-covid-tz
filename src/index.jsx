095b1cddff55ae7c2411c2c09ece495490dbc3c8
DIma <dagassacoop@gmail.com>
2021-08-21 17:51:58 +0300

    Сделан вывод информации в всплывающее окно

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,10 @@ import {render} from 'react-dom'
 // const data = getData()
 
 
+import TotalConfirmed from '@/assets/total-confirmed-img.png'
+import TotalDeaths from '@/assets/total-deaths-img.png'
+import TotalRecovered from '@/assets/total-recovered-img.png'
+
 
 class App extends React.Component { 
     render(){
@@ -38,9 +42,17 @@ class Main extends React.Component {
         this.state = {
             data: Object,
             dataCountrys: [],
-            dataGlobal: []
+            dataGlobal: [],
+            popUpStatus: false,
+            countryTitle: 'some country',
+            countryConfirmed: 0,
+            countryDeaths: 0,
+            countryRecovered: 0
         }
         this.getData()
+        
+        this.getPupUp = this.getPupUp.bind(this)
+        this.updateData = this.updateData.bind(this)
     }
 
     getData(){
@@ -53,17 +65,48 @@ class Main extends React.Component {
     }
 
 
+    getPupUp(e){
+        let targetDiv = e.target
+        if(targetDiv.className === 'main__list-item'){
+            console.log(targetDiv)
+            this.updateStatePopUp(targetDiv)
+        }else{
+            targetDiv = targetDiv.parentNode
+            if(targetDiv.className === 'main__list-item'){
+                console.log(targetDiv)
+                this.updateStatePopUp(targetDiv)
+            }
+        }
+        console.log('start')
+    }
+
+    updateStatePopUp(targetDiv){
+        this.setState(
+            {
+                popUpStatus: true, 
+                countryTitle: targetDiv.getAttribute('data-title'),
+                countryConfirmed: targetDiv.getAttribute('data-totalconfirmed'),
+                countryDeaths: targetDiv.getAttribute('data-totaldeaths'),
+                countryRecovered: targetDiv.getAttribute('data-totalrecovered')
+            }
+        )
+    }
+
+    updateData = (value) => {
+        this.setState({popUpStatus: value})
+        console.log('exit')
+    }
 
     render(){    
-        console.log(this.state.dataCountrys)
         return(
             <main className="main">
-                <ul className="main__list">
+                <ul className="main__list" onClick={this.getPupUp}>
                     <ListItem title={true}/>
                     <div className="main__list-content" id="mainListContent">
-                        {this.state.dataCountrys.map((country,i) => (<ListItem key={i} index={i+1} country={country.Country} total={country.TotalConfirmed} />))}
+                        {this.state.dataCountrys.map((country,i) => (<ListItem key={i} index={i+1} country={country.Country} totalConfirmed={country.TotalConfirmed} totalDeaths={country.TotalDeaths} totalRecovered={country.TotalRecovered} />))}
                     </div>
                 </ul>
+                {this.state.popUpStatus && <PopUp updateData={this.updateData} title={this.state.countryTitle} totalConfirmed={this.state.countryConfirmed} totalDeaths={this.state.countryDeaths} totalRecovered={this.state.countryRecovered} />}
             </main>
         )
     }
@@ -72,28 +115,128 @@ class Main extends React.Component {
 class ListItem extends React.Component {
     constructor (props){
         super(props)
-        this.className = 'main__list-item '
+        this.className = 'main__list-item'
         this.index = this.props.index
         this.country = this.props.country
-        this.total = this.props.total
+        this.totalConfirmed = this.props.totalConfirmed
         if(this.props.title){
-            this.className += 'main__list-item_title'
+            this.className += ' main__list-item_title'
             this.index = '№'
             this.country = 'Country'
-            this.total = 'Total Confirmed'
+            this.totalConfirmed = 'Total Confirmed'
         }
+
     }
+
+   
+
     render(){
+        // console.log(this.country)
         return (
-            <ul className={this.className}>
+            <ul className={this.className} data-title={this.country} data-totalconfirmed={this.props.totalConfirmed} data-totaldeaths={this.props.totalDeaths} data-totalrecovered={this.props.totalRecovered}>
                 <li className="main__list-item-col">{this.index ? this.index : '-'}</li>
                 <li className="main__list-item-col_line"></li>
                 <li className="main__list-item-col">{this.country ? this.country : '-'}</li>
                 <li className="main__list-item-col_line"></li>
-                <li className="main__list-item-col">{this.total ? this.total : '-'}</li>
+                <li className="main__list-item-col">{this.totalConfirmed ? this.totalConfirmed : '-'}</li>
             </ul>
         )
     }
 }
 
+class PopUp extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div className="pop-up">
+                <div className="pop-up__bg"></div>
+                <div className="pop-up__body">
+                    <h2 className="pop-up__title">{this.props.title}</h2>
+                    <div className="pop-up__content">
+                        <PopUpContentRow img={TotalConfirmed} title="Total Confirmed" value={this.props.totalConfirmed} />
+                        <PopUpContentRow img={TotalDeaths} title="Total Deaths" value={this.props.totalDeaths} />
+                        <PopUpContentRow img={TotalRecovered} title="Total Recovered" value={this.props.totalRecovered} />
+                    </div>
+                    <button className="pop-up__button" onClick={() => {this.props.updateData(false)}} >OK</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+class PopUpContentRow extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div className="pop-up__content-row">
+                <img className="pop-up__content-row-img" src={this.props.img} alt="" />
+                <p>{this.props.title}</p>
+                <p>{this.props.value}</p>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+class TestButton extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            popUpStatus: false
+        }
+        this.getPupUp = this.getPupUp.bind(this)
+        this.updateData = this.updateData.bind(this)
+
+    }
+    getPupUp(){
+        this.setState({popUpStatus: true})
+    }
+
+    updateData = (value) => {
+        this.setState({popUpStatus: value})
+    }
+
+    render(){
+        return(
+            <div className="div">
+                <button className="test-button" onClick={this.getPupUp}>Show</button>
+                {this.state.popUpStatus && <PopUp updateData={this.updateData} title="Alabama" TotalConfirmed="100" TotalDeaths="1000" TotalRecovered="10000" />}
+            </div>   
+        )
+    }
+}
+
+
+
 render(<App />,document.getElementById('app'))
+// render(<TestButton />,document.getElementById('app'))
+
+
+
+
+
+
+
+
+
+
+
+
+
